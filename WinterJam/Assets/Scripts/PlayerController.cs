1b0d9fb35d89ae4e9a24994a93c69507d78ef757
DcentGaming <dabarker2000@yahoo.com>
2025-12-23 22:23:51 -0500

    updated PlayerController
    Added rotation for looking with mouse

--- WinterJam/Assets/Scripts/PlayerController.cs
+++ WinterJam/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,21 @@ public class PlayerController : MonoBehaviour
 {
     private PlayerControls controls;
     private CharacterController controller;
+    private Camera playerCam;
+    private float pitch;
 
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float turnSpeed;
+    [SerializeField] private float minPitchAngle;
+    [SerializeField] private float maxPitchAngle;
 
     private void Awake()
     {
-        controls = new PlayerControls();                    //Set up input system
-        controller = GetComponent<CharacterController>();   //Get Character Controller
+        controls = new PlayerControls();                    // Set up input system
+        controller = GetComponent<CharacterController>();   // Get Character Controller
+        playerCam = GetComponentInChildren<Camera>();       // Get Camera
+
+        pitch = 0.0f;
     }
 
     private void OnEnable()
@@ -26,13 +34,25 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         movePlayer();
+        Look();
     }
 
     void movePlayer()
     {
-        Vector2 input = controls.Player.Move.ReadValue<Vector2>();  //Grab the input
+        Vector2 input = controls.Player.Move.ReadValue<Vector2>();  // Grab move input
         Vector3 move = new Vector3(input.x, 0, input.y);
-        move = transform.TransformDirection(move);                  //Transform dir from local to world
+        move = transform.TransformDirection(move);                  // Transform dir from local to world //Relative to look pos
         controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+
+    void Look()
+    {
+        Vector2 input = controls.Player.Look.ReadValue<Vector2>();  // Grab look input
+        // Yaw
+        transform.Rotate(Vector3.up, input.x * turnSpeed * Time.deltaTime);
+        // Pitch
+        pitch -= input.y * turnSpeed * Time.deltaTime;
+        pitch = Mathf.Clamp(pitch, minPitchAngle, maxPitchAngle);
+        playerCam.transform.localRotation = Quaternion.Euler(pitch, 0, 0);
     }
 }
